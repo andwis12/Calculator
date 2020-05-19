994f12d2202f78807f0d3aa75b5c72442fd3270b
andwis12 <1bandrzejwisinski@gmail.com>
2020-05-19 23:09:16 +0200

    Basic layout

---
+++ app/src/main/java/com/andrzej/calculator/SimpleCalculatorActivity.java
@@ -0,0 +1,15 @@
+package com.andrzej.calculator;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SimpleCalculatorActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_calculator_layout);
+    }
+
+}
